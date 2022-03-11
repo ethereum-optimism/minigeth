3a3b02c77830cfb40525b0be9f9cdce96da2d705
Nicolas "Norswap" Laurent <norswap@gmail.com>
2022-03-11 10:26:14 +0100

    modify and add files in crypto directory

--- crypto/blake2b/blake2b_ref.go
+++ crypto/blake2b/blake2b_ref.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !amd64 appengine gccgo
-
 package blake2b
 
 func f(h *[8]uint64, m *[16]uint64, c0, c1 uint64, flag uint64, rounds uint64) {
--- crypto/signature_nocgo.go
+++ crypto/signature_nocgo.go
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// +build nacl js !cgo gofuzz
-
 package crypto
 
 import (
@@ -25,7 +23,7 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/btcsuite/btcd/btcec"
+	"github.com/ethereum/go-ethereum/crypto/btcec"
 )
 
 // Ecrecover returns the uncompressed public key that created the given signature.
