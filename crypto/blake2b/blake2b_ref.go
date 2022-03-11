bcd3933b16e6e252118f45139c1adf15dc153dc0
Nicolas "Norswap" Laurent <norswap@gmail.com>
2022-03-11 10:26:14 +0100

    modify and add files in crypto directory

--- crypto/blake2b/blake2b_ref.go
+++ crypto/blake2b/blake2b_ref.go
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !amd64 || appengine || gccgo
-// +build !amd64 appengine gccgo
-
 package blake2b
 
 func f(h *[8]uint64, m *[16]uint64, c0, c1 uint64, flag uint64, rounds uint64) {
